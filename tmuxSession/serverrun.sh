c6df2b5b8713dc3ba1a3b9e618921b6a9de493a8
Thomas Rea (HP Proliant - Ubuntu Server 16.04) <thomasrea0113@gmail.com>
2017-02-10 16:08:37 -0500

    implement io logging

--- tmuxSession/serverrun.sh
+++ tmuxSession/serverrun.sh
@@ -7,9 +7,18 @@ fi
 rm -f /tmp/sterr
 mkfifo /tmp/sterr
 
-tmux new-session -ds test sh -c './testTmux.sh 2>/tmp/sterr; tmux kill-session -t test'
+rm -f /tmp/stin.log
+touch /tmp/stin.log
+
+rm -f /tmp/stout.log
+touch /tmp/stout.log
+
+rm -f /tmp/sterr.log
+touch /tmp/sterr.log
+
+tmux new-session -ds test bash -c './testTmux.sh 2> >(tee /tmp/sterr.log >/tmp/sterr) | tee /tmp/stout.log; tmux kill-session -t test'
 tmux split-window -ht test.0 
-tmux send-keys -t test.1 "s(){ tmux send-keys -t test.0 \"\$(echo \"\${@:1}\")\" ENTER; }" ENTER 'clear' ENTER
+tmux send-keys -t test.1 "s(){ tmux send-keys -t test.0 \"\$(echo \"\${@:1}\" | tee -a /tmp/stin.log)\" ENTER; }" ENTER 'clear' ENTER
 tmux split-window -t test.0 cat /tmp/sterr
 
 tmux attach -t test.1
